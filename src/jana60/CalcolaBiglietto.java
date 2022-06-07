e53a4548e68bb7635526e72c734e9e29b4666a82
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-07 15:29:55 +0200

    secondo commit

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -1,5 +1,31 @@
 package jana60;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		
+		
+		
+		
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Quanti Kilometri vuou percorrere?");
+		int kilometriDaPercorrere = scan.nextInt();
+		
+		System.out.println("Quanti hanni ha il passeggero ?");
+		int EtaPasseggero = scan.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		
+		  scan.close();
+		
+		
+		
+	}
 
 }
